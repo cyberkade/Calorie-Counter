3c7bbee3634da5c11dd11aac72a12b3fc3f0030b
Kade Griffith <kadeg95@gmail.com>
2021-07-16 14:22:06 -0600

    created assignGoal function that appends all the users inputs onto the screen as a visual towards tehir set goal

--- app.js
+++ app.js
@@ -11,24 +11,54 @@ goalForm.addEventListener('submit', (e) => {
     toggleHide('#display-section');
     toggleHide('#goal-form');
     toggleHide('#food-form');
+    toggleHide('.list-section');
+    assignGoals('.append','.goal-total',4);
     // displayGoals();
   });
 foodForm.addEventListener('submit', (e) =>{
     e.preventDefault();
-    createList();
+    listItem();
+
+    foodForm.elements.calories.value = "";
+    foodForm.elements.carbs.value = "";
+    foodForm.elements.proteins.value = "";
+    foodForm.elements.fats.value = "";
+    foodForm.elements.food.value = "";
+    foodForm.elements.servings.value = "";
 })
-const createList = ()=>{
-  const item = foodForm.elements.food.value;
-  const food = document.createElement("li");
-  const ateToday = document.querySelector(".list-section");
-  const foodList = document.querySelector("ul");
-  ateToday.classList.remove("hide");
-  food.classList.add("list");
-  food.append(item);
-  foodList.append(food);
+const listItem = ()=>{
+  const userInput = foodForm.elements.food.value;
+  const listItem = document.createElement("li");
+  const list = document.querySelector("ul");
+  listItem.classList.add("list");
+  listItem.append(userInput);
+  list.append(listItem);
   }
 const toggleHide = (id) =>{
   const display = document.querySelector(id);
   display.classList.toggle('hide');
-  console.log(display);
 }
+
+// parent is the element you want the object appended to--- num is the amount of parent objects you want to have the element appended to.
+const assignGoals = (parent1, parent2, num) => {
+// replace 'goalCal, goalCarbs, etc.' with input's name you want to grab information from'
+  const calories = goalForm.elements.goalCal.value;
+  const carbs = goalForm.elements.goalCarbs.value;
+  const proteins = goalForm.elements.goalPro.value;
+  const fats = goalForm.elements.goalFats.value;
+  const pTotal = document.querySelectorAll(parent2);
+  const pGoal = document.querySelectorAll(parent1);
+  const array = [calories, carbs, proteins, fats] 
+  for(let i = 0; i < num; i++){
+    pGoal[i].append(array[i]);
+    pTotal[i].append(0);
+    console.log(pTotal);
+  }
+}
+const updateGoals = ()=>{
+
+
+
+  pTotal[i].append(total.calories);
+}
+// assignGoals('.append','goal-total',4);
