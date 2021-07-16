ed1652875e32d498b9ea086a5775a7c696aef98c
Kade Griffith <kadeg95@gmail.com>
2021-07-16 12:14:28 -0600

    worked on styles of the site is every state. Wrote functions to toggle hide, and to create the list of foods eaten

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const goalForm = document.querySelector("#goalForm");
-const foodForm = document.querySelector("#foodForm");
+const goalForm = document.querySelector("#goal-form");
+const foodForm = document.querySelector("#food-form");
 let total = {
   carbs: 0,
   calories: 0,
@@ -8,21 +8,27 @@ let total = {
 }
 goalForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    assignGoals();
+    toggleHide('#display-section');
+    toggleHide('#goal-form');
+    toggleHide('#food-form');
     // displayGoals();
   });
 foodForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     createList();
 })
-  const createList = ()=>{
-    const item = foodForm.elements.food.value;
-    const food = document.createElement("li");
-    const ateToday = document.querySelector("h2");
-    const foodList = document.querySelector("ul");
-
-    ateToday.classList.remove("hide");
-    food.classList.add("list");
-    food.append(item);
-    foodList.append(food);
+const createList = ()=>{
+  const item = foodForm.elements.food.value;
+  const food = document.createElement("li");
+  const ateToday = document.querySelector(".list-section");
+  const foodList = document.querySelector("ul");
+  ateToday.classList.remove("hide");
+  food.classList.add("list");
+  food.append(item);
+  foodList.append(food);
   }
+const toggleHide = (id) =>{
+  const display = document.querySelector(id);
+  display.classList.toggle('hide');
+  console.log(display);
+}
